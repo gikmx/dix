2c9d324da3887c6c90e0f91e7507ac800855c9e9
Héctor Menéndez <etor@gik.mx>
2016-04-06 14:59:05 -0500

    Moved bin methods to sys and added support for brew.

--- lib/dotfiles/bin.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-[ -z "$DOTFILES" ] && echo "Invalid enviromnent." && exit
-
-# Test if the command ($1) is available
-bin(){
-	if `command -v $1 > /dev/null 2>&1`; then return 0; else return 1; fi
-}
