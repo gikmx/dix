7364dda557bfbd6775cfe0379f9a00287cc6e111
Hector Menendez <etor@gik.mx>
2016-04-09 02:41:10 -0500

    moved core libs to /lib's root

--- lib/dix/set.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-! $DIX && >&2 echo "DIX404" && exit 1
-
-# Take ownership of target ($1)
-set.ownership() {
-    sudo -k chown -R $(id -u):$(id -g) $1
-    sudo -k chmod -R u+rw,g+u,o-rwx $1
-}
