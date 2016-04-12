fa7e23a439dadb7c35a73a8c966d0af019babb39
Héctor Menéndez <etor@gik.mx>
2016-04-12 02:12:36 -0500

    Packaging library reimplemented, fetch method only for the moment.

--- lib/pkg.sh
+++ lib/pkg.sh
@@ -1,27 +1,48 @@
 #! /usr/bin/env bash
 [ ! $DIX ] && >&2 echo "${BASH_SOURCE[0]}:DIX404" && exit 1
 
-pkg.exists(){
-	[ -z "$1" ] && log.error "PKG_EXISTS_406" && return 1
-	! test -z "`grep "$1" "$DIX_PATH_SRV/DIX_PKGS"`"
+pkg.__parts(){
+    [[ ! $1 ]] && log.error "Missing package name" && exit 1
+    local parts
+    # Split string into an array separated by ":" so we can validate format
+    IFS=':' read -r -a parts <<< "$1"
+    [[ ${#parts[@]} != 2 ]] && log.error "Invalid package name $1" && exit 1
+    echo "${1//:/ }"
 }
 
-pkg.enable(){
-	[ -z "$1" ] && log.error "PKG_ENABLE_406" && exit 1
-	local filename="$DIX_PATH_SRV/DIX_PKGS"
-	[ ! -f "$filename" ] && touch "$filename"
-	# Append the current package and remove duplicates
-	echo "$1" >> "$filename"
-	echo "`sort -u "$filename"`" > $filename
-	log.info "Enabled Package $1"
+pkg.__names(){
+    [[ -z "$@" ]] && log.error "Expected package name(s)" && exit 1
+    local names=()
+    local parts
+    for pkg in $@; do names+=("$(pkg.__parts $pkg)"); done
+    # echoes the variable declaration, so it can be evaulated on calling func.
+    # This is the only way I've found to return an array
+    declare -p names
 }
 
-pkg.disable(){
-	local filename="$DIX_PATH_SRV/DIX_PKGS"
-	[ ! -f "$filename" ] && touch "$filename"
-	[ -z "$1" ] && log.error "PKG_DISABLE_406" && exit 1
-	# Outputs the inverse of the matched file
-	echo "$(sed  -n "/$1/!p" "$filename")" > "$filename"
-	log.info "Disabled Package $1"
+pkg.__repo(){
+    [[ ! $1 || ! $2 ]] && log.error "Expected package array" && exit 1
+    echo "git://github.com/$1/dix-$2.git"
 }
 
+pkg.__path(){
+    [[ ! $1 || ! $2 ]] && log.error "Expected package array" && exit 1
+    echo "$DIX_PATH_OPT/$1-$2"
+}
+
+pkg.fetch(){
+    # Make sure git is available before doing anything
+    ! sys.has "git" && log.error "Git could not be found" && exit 1
+    # Populate and validate package names
+    local names pack repo path
+    eval $(pkg.__names $@)
+    # Iterate into sent packages and fetch the repo
+    for pack in "${names[@]}"; do
+        pack=($pack) # convert it to array
+        repo="`pkg.__repo ${pack[0]} ${pack[1]}`"
+        path="`pkg.__path ${pack[0]} ${pack[1]}`"
+        [[ -d $path ]] && rm -Rf $path
+        git clone $repo $path ||\
+            log.error "Could not install: ${pack[@]}" && exit 1
+    done
+}
