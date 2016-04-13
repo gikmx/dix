e8222fd5c66465e6a776683d89165b2685b01109
Héctor Menéndez <etor@gik.mx>
2016-04-13 05:14:18 -0500

    Created «Enable & Disable» methods for pkg

--- lib/pkg.sh
+++ lib/pkg.sh
@@ -20,52 +20,112 @@ pkg.__names(){
     declare -p names
 }
 
-pkg.__repo(){
-    [[ ! $1 || ! $2 ]] && log.error "Expected package array" && exit 1
-    echo "git://github.com/$1/dix-$2.git"
+pkg.info(){
+    [[ ! $1 ]] && log.error "Invalid package parts" && exit 1
+    pack=($1) # converts it to array
+    echo "${pack[0]}-${pack[1]}"
 }
 
-pkg.__path(){
-    [[ ! $1 || ! $2 ]] && log.error "Expected package array" && exit 1
-    echo "$DIX_PATH_OPT/$1-$2"
+pkg.info.repo(){
+    [[ ! $1 ]] && log.error "Invalid package parts" && exit 1
+    pack=($1) # converts it to array
+    echo "git://github.com/${pack[0]}/dix-${pack[1]}.git"
+}
+
+pkg.info.root(){
+    local name=$(pkg.info "$1")
+    echo "$DIX_PATH_OPT/$name"
+}
+
+pkg.info.path(){
+    local name=$(pkg.info "$1")
+    echo "$DIX_PATH_BOOT/$name"
 }
 
 pkg.fetch(){
     # Make sure git is available before doing anything
     ! sys.has "git" && log.error "Git could not be found" && exit 1
     # Populate and validate package names
-    local names pack repo path
+    local names pack repo root
     eval $(pkg.__names $@)
     # Iterate into sent packages and fetch the repo
     for pack in "${names[@]}"; do
-        pack=($pack) # convert it to array
-        repo="$(pkg.__repo ${pack[0]} ${pack[1]})"
-        path="$(pkg.__path ${pack[0]} ${pack[1]})"
-        [[ -d $path ]] && rm -Rf $path
-        git clone $repo $path ||\
-            log.error "Could not install: ${pack[@]}" && exit 1
+        repo="$(pkg.info.repo $pack)"
+        root="$(pkg.info.root $pack)"
+        [[ -d $root ]] && rm -Rf $root
+        git clone $repo $root ||\
+            log.error "Could not install: $(pkg.info $pack)" && exit 1
     done
 }
 
 pkg.enable(){
-    local names
+    local names DIX_PKG DIX_PKG_REPO DIX_PKG_ROOT DIX_PKG_PATH
     eval $(pkg.__names $@)
     for pack in "${names[@]}"; do
-        pack=($pack)
-        DIX_PKG_GROUP=${pack[0]}
-        DIX_PKG_NAME=${pack[1]}
-        DIX_PKG_REPO="`pkg.__repo ${pack[0]} ${pack[1]}`"
-        DIX_PKG_PATH="`pkg.__path ${pack[0]} ${pack[1]}`"
-        [[ ! -d "$DIX_PKG_PATH" ]] &&\
-            log.error "Package not available: ${pack[@]}" && exit 1
-        # Read configuration file
-        [[ ! -f "$DIX_PKG_PATH/boot.conf" ]] &&\
-            log.error "Invalid conf: ${pack[@]}" && exit 1
-        (
-            source "$DIX_PKG_PATH/boot.conf"
-            [[ $name != $DIX_PKG_NAME ]] &&\
-                log.error "Invalid package name: $name" && exit 1
+        DIX_PKG="$(pkg.info "$pack")"
+        DIX_PKG_REPO="$(pkg.info.repo "$pack")"
+        DIX_PKG_ROOT="$(pkg.info.root "$pack")"
+        DIX_PKG_PATH="$(pkg.info.path "$pack")"
 
-        )
+        [[ ! -d "$DIX_PKG_ROOT" ]] &&\
+            log.error "Package not available: $DIX_PKG" && exit 1
+
+        [[ -d "$DIX_PKG_PATH" ]] && rm -Rf "$DIX_PKG_PATH"
+
+        # Find all files inside the folder, but omit platform specific
+        # iterate over them and determine if a platform specific file exist
+        # if it does, then append it to the end of line
+        cmd=(find $DIX_PKG_ROOT -type f ! -name "README.md" ! -path "$DIX_PKG_ROOT/.*")
+        for platform in $(sys.supported); do cmd+=(! -name "*.$platform"); done
+        for origname in $("${cmd[@]}"); do
+            bootname=${origname/$DIX_PKG_ROOT/$DIX_PKG_PATH}
+            [[ ! -d $(dirname "$bootname") ]] && mkdir -p $(dirname "$bootname")
+            cp "$origname" "$bootname"
+            # does a platform specific file exist? append it to normal file.
+            platname="$origname.$(sys.name)"
+            [[ -f $platname ]] && echo -e "\n$(cat $platname)\n" >> $bootname
+            log.info "Copied: $DIX_PKG${bootname/$DIX_PKG_PATH/}"
+        done
+        unset cmd platform origname bootname platname
+
+        # With files in place, it's now time to symlink them into dix
+        for path in $(find $DIX_PKG_PATH -maxdepth 1 -type d ! -path $DIX_PKG_PATH); do
+            dest="DIX_PATH_$(string.upper ${path/$DIX_PKG_PATH\//})"
+            [[ -z "${!dest}" ]] && continue
+            dest=${!dest}
+            rm -rf "$dest/$DIX_PKG"
+            ln -s "$path" "$dest/$DIX_PKG"
+            log.info "Linked: $DIX_PKG${path/$DIX_PKG_PATH/}"
+        done
+        unset path dest
+
+        log.info "Enabled: $DIX_PKG"
     done
+}
+
+pkg.disable(){
+    eval $(pkg.__names $@)
+    local paths path did
+    for pack in "${names[@]}"; do
+        DIX_PKG="$(pkg.info "$pack")"
+        paths=$(find "$DIX_PATH" ! -type f ! -path "$DIX_PATH_OPT/*" -name "$DIX_PKG")
+        did=false
+        for path in $paths; do
+            rm -Rf "$path" && log.info "Removed ${path/$DIX_PATH\//}" && did=true
+        done
+        $did && log.info "Disabled: $DIX_PKG"
+    done
+}
+
+pkg.install(){
+    # TODO: Finish this
+    # Read configuration file
+    [[ ! -f "$DIX_PKG_ROOT/boot.conf" ]] &&\
+        log.error "Invalid conf: $DIX_PKG" && exit 1
+    (
+        source "$DIX_PKG_ROOT/boot.conf"
+        [[ $name != $DIX_PKG ]] &&\
+            log.error "Invalid package name: $name" && exit 1
+
+    )
 }
