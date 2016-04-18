249f3b1896896c5f3475efddfb85e01545879eef
Hector Menendez <etor@gPro.local>
2016-04-18 14:23:41 -0500

    Moved files in lib,etc & sbin into opt/dix so it follows the same enabling processs as every other package.

--- opt/dix/lib/dix.sh
+++ opt/dix/lib/dix.sh
@@ -20,7 +20,7 @@ dix.error(){
 }
 
 # Convert directories in $1 into files in $2
-dix.env_set(){
+dix.env.save(){
 	[[ ! $1 || ! $2 ]] && >&2 echo "ERR:dix.set_srv" && exit 1
 	local path name
 	# Set the path environment variables
@@ -28,11 +28,12 @@ dix.env_set(){
 	    name="DIX_PATH_$(basename $path | tr '[:lower:]' '[:upper:]')"
 	    echo "$path" > "$2/$name"
 	done
-	echo "$DIX_PATH" > "$2/DIX_PATH"
+	echo "$2" > "$2/DIX_PATH_ENV"
+	echo "$1" > "$2/DIX_PATH"
 }
 
 # Populates the environment using files on $1
-dix.env(){
+dix.env.load(){
 	[ ! $1 ] && >&2 echo "dix.srv:404" && exit 1
 	local name
 	for path in $(find $1 -maxdepth 1 -type f ! -name "README.md"); do
@@ -42,7 +43,7 @@ dix.env(){
 }
 
 # load libraries on $1
-dix.lib(){
+dix.load.lib(){
 	[ ! $1 ] && >&2 echo "dix.srv:404" && exit 1
 	for DIX_LIB_PATH in $(find $1 -type f -name "*.sh" ! -name "dix.sh"); do
 		DIX_LIB_NAME=$(basename $DIX_LIB_PATH)
@@ -54,29 +55,9 @@ dix.lib(){
 	done
 }
 
-# TODO:
-dix.dotfiles(){
-	pkgs="$(cat $DIX_PATH_SRV/DIX_PKGS)"
-
-	export PATH=$DIX_PATH_BIN:$DIX_PATH_SBIN:$PATH
-
-	for pkg in ${pkgs[@]}; do
-		dir=$DIX_PATH_BOOT/$pkg
-		[ ! -d $dir ] && dix.error "PKG404:$pkg"
-		[ ! -f "$dir/profile" ] && continue
-		source "$dir/profile"
-		[ ! -f "$dir/profile.$(sys.name)" ] && continue
-		source "$dir/profile.$(sys.name)"
-		if type.is_func DIX_ON_AFTER; then
-			DIX_ON_AFTER $DIX_PKG
-			unset DIX_ON_AFTER
-		fi
-	done
-}
-
 dix.load(){
-	dix.env $DIX_PATH/srv
-	dix.lib $DIX_PATH/lib
+	dix.load.srv $DIX_PATH/srv
+	dix.load.lib $DIX_PATH/lib
 	# dix.etc $DIX_PATH/etc
 }
 
--- srv/README.md
+++
@@ -1,5 +0,0 @@
-## /srv
-
-- Environment variables, everything here will be populated to an envvar
-- One fle per envvar, no extensions, all caps.y
-
