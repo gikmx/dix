7d65108a7f0753a603369b5618eb1e5877f8e5f9
Héctor Menéndez <etor@gMac.local>
2016-04-19 04:12:52 -0500

    splitted and simplified pkg lib.

--- opt/dix/lib/pkg.sh
+++ opt/dix/lib/pkg.sh
@@ -1,188 +1,78 @@
 #! /usr/bin/env bash
 [ ! $DIX ] && >&2 echo "${BASH_SOURCE[0]}:DIX404" && exit 1
 
-pkg.__parts(){
-    [[ ! $1 ]] && dix.throw "Missing package name"
-    local parts
-    # Split string into an array separated by ":" so we can validate format
-    IFS='-' read -r -a parts <<< "$1"
-    [[ ${#parts[@]} != 2 ]] && dix.throw "Invalid package name $1"
-    echo "${1//-/ }"
-}
-
-pkg.__names(){
+pkg.parse(){
     [[ -z "$@" ]] && dix.throw "Expected package name(s)"
-    local names=()
-    local parts
+    local pkgs=()
     for pkg in $@; do
         parts="$(pkg.__parts $pkg)" || exit 1
-        names+=("$parts")
+        # Appends an array declaration to the pkgs array
+        pkgs+=("$(string.split "$pkg" '-' 'pkg')")
     done
-    # echoes the variable declaration, so it can be evaulated on calling func.
-    # This is the only way I've found to return an array
-    declare -p names
+    # Return the array of arrays declaration so it can be evaluated
+    declare -p pkgs
 }
 
-pkg.__boothalt(){
-    local TYPE=$1 && shift
-    local names pack
-    names=$(pkg.__names $@) && eval $names || exit 1
-    # run each boot script in a sub shell.
-    for pack in "${names[@]}"; do (
-        DIX_PKG="$(pkg.info "$pack")"
-        DIX_PKG_REPO="$(pkg.info.repo "$pack")"
-        DIX_PKG_ROOT="$(pkg.info.root "$pack")"
-        DIX_PKG_PATH="$(pkg.info.path "$pack")"
-        # TODO: Implement the sub installation process.
-        [[ ! -f "$DIX_PKG_PATH/$TYPE" ]] &&\
-            dix.log "Not found for $TYPE: $DIX_PKG" && continue
-        # load the configuration
-        [[ ! -f "$DIX_PKG_PATH/boot.conf" ]] && dix.throw "Missing conf: $DIX_PKG"
-        source "$DIX_PKG_PATH/boot.conf" || dix.throw "Invalid conf: $DIX_PKG"
-        # make sure an array specifying the dependencies is declared
-        ! type.is_array DIX_REQUIRE && dix.throw "Invalid conf (required array): $DIX_PKG"
-        # Install subdependencies:
-        # TODO: Handle uninstallation of dependencies
-        [[ "$TYPE"  == 'boot' && ${#DIX_REQUIRE[@]} -gt 0 ]] &&\
-            pkg.install ${DIX_REQUIRE[@]}
-        # Everything ready, run the script
-        source "$DIX_PKG_PATH/$TYPE"
-        type.is_func DIX_ON_AFTER && DIX_ON_AFTER
-    ); done
-}
-
-pkg.info(){
+pkg.info.name(){
     [[ ! $1 ]] && dix.throw "Invalid package parts"
-    pack=($1) # converts it to array
-    echo "${pack[0]}-${pack[1]}"
+    local pkg && eval $1
+    echo "${pkg[0]}-${pkg[1]}"
 }
 
 pkg.info.repo(){
     [[ ! $1 ]] && dix.throw "Invalid package parts"
-    pack=($1) # converts it to array
-    echo "git://github.com/${pack[0]}/dix-${pack[1]}.git"
+    local pkg && eval $1
+    echo "git://github.com/${pkg[0]}/dix-${pkg[1]}.git"
 }
 
 pkg.info.root(){
-    local name=$(pkg.info "$1")
+    local name=$(pkg.info.name "$1")
     echo "$DIX_PATH_OPT/$name"
 }
 
 pkg.info.path(){
-    local name=$(pkg.info "$1")
+    local name=$(pkg.info.name "$1")
     echo "$DIX_PATH_BOOT/$name"
 }
 
-pkg.fetch(){
-    # Make sure git is available before doing anything
-    ! sys.has "git" && dix.throw "Git could not be found"
-    # Populate and validate package names
-    local names name pack repo root
-    names=$(pkg.__names $@) && eval $names || exit 1
-    # Iterate into sent packages and fetch the repo
-    for pack in "${names[@]}"; do
-        name="$(pkg.info "$pack")"
-        repo="$(pkg.info.repo "$pack")"
-        root="$(pkg.info.root "$pack")"
-        [[ -d $root ]] && dix.log "Already fetched: $name" && continue
-        git clone $repo $root ||\
-            dix.throw "Could not install: $(pkg.info "$pack")"
-        dix.log "Fetched: $name"
-    done
+pkg.boot.check(){
+    local DIX_PKG_TYPE=$2
+    local DIX_PKG_PATH=$1
+    local DIX_PKG=$(echo ${DIX_PKG_PATH/$DIX_DIX_PKG_PATH_BOOT\/} | perl -pe 's/^\d+\-//')
+    local DIX_ERR="$DIX_PKG/$DIX_PKG_TYPE.conf"
+
+    [[ ! -f "$DIX_PKG_PATH/$DIX_PKG_TYPE" ]] && dix.log "$DIX_ERR:404" && return 1
+    [[ ! -f "$DIX_PKG_PATH/$DIX_PKG_TYPE.conf" ]] && dix.throw "$DIX_ERR.conf:404"
+
+    source "$DIX_PKG_PATH/$DIX_PKG_TYPE.conf" || dix.throw "$DIX_ERR.conf:500"
+    [[ -z "$priority" ]] && dix.throw "$DIX_ERR.conf:500 Invalid priority"
+    ! type.is_array requires && dix.throw "$DIX_ERR.conf:500 Invalid requires"
+    local result=()
 }
 
-pkg.lose(){
-    local names pack did name root
-    names=$(pkg.__names $@) && eval $names || exit 1
-    for pack in "${names[@]}"; do
-        name="$(pkg.info "$pack")"
-        root="$(pkg.info.root "$pack")"
-        [[ ! -d $root ]] && dix.log.error "Not found: $name" && continue
-        rm -Rf "$root" && dix.log "Lost: ${root/$DIX_PATH_OPT\//}"
-    done
-}
+pkg.boot.run(){
+    local names pkg cmd DIX_PKG DIX_PKG_REPO DIX_PKG_ROOT DIX_PKG_PATH
+    local DIX_PKG_TYPE=$1 && shift
+    pkgs=$(pkg.parse $@) && eval $pkgs || exit 1
+    # run each boot script in a sub shell.
+    for pkg in "${pkgs[@]}"; do (
+        DIX_PKG="$(pkg.info.name "$pkg")"
+        DIX_PKG_REPO="$(pkg.info.repo "$pkg")"
+        DIX_PKG_ROOT="$(pkg.info.root "$pkg")"
+        DIX_PKG_PATH="$(pkg.info.path "$pkg")"
 
-pkg.enable(){
-    local names pack cmd DIX_PKG DIX_PKG_REPO DIX_PKG_ROOT DIX_PKG_PATH
-    names=$(pkg.__names $@) && eval $names || exit 1
-    for pack in "${names[@]}"; do
-        DIX_PKG="$(pkg.info "$pack")"
-        DIX_PKG_REPO="$(pkg.info.repo "$pack")"
-        DIX_PKG_ROOT="$(pkg.info.root "$pack")"
-        DIX_PKG_PATH="$(pkg.info.path "$pack")"
+        ! pkg.boot.check "$DIX_PKG_PATH" "$DIX_PKG_TYPE" && continue
 
-        [[ ! -d "$DIX_PKG_ROOT" ]] &&\
-            dix.throw "Package not available: $DIX_PKG"
+        # handle subdependencies first
+        [[ "$DIX_PKG_TYPE" == 'boot']] && cmd='install' || cmd='uninstall'
+        [[ ${#requires}]]
 
-        [[ -d "$DIX_PKG_PATH" ]] && rm -Rf "$DIX_PKG_PATH"
-
-        # Find all files inside the folder, but omit platform specific
-        # iterate over them and determine if a platform specific file exist
-        # if it does, then append it to the end of line
-        cmd=(find $DIX_PKG_ROOT -type f ! -name "README.md" ! -path "$DIX_PKG_ROOT/.*")
-        for platform in $(sys.supported); do cmd+=(! -name "*.$platform"); done
-        for origname in $("${cmd[@]}"); do
-            bootname=${origname/$DIX_PKG_ROOT/$DIX_PKG_PATH}
-            [[ ! -d $(dirname "$bootname") ]] && mkdir -p $(dirname "$bootname")
-            cp "$origname" "$bootname"
-            # does a platform specific file exist? append it to normal file.
-            platname="$origname.$(sys.name)"
-            [[ -f $platname ]] && echo -e "\n$(cat $platname)\n" >> $bootname
-            dix.log "Copied: $DIX_PKG${bootname/$DIX_PKG_PATH/}"
-        done
-        unset cmd platform origname bootname platname
-
-        # With files in place, it's now time to symlink them into dix
-        for path in $(find $DIX_PKG_PATH -maxdepth 1 -type d ! -path $DIX_PKG_PATH); do
-            dest="DIX_PATH_$(string.upper ${path/$DIX_PKG_PATH\//})"
-            [[ -z "${!dest}" ]] && continue
-            dest=${!dest}
-            rm -rf "$dest/$DIX_PKG"
-            ln -s "$path" "$dest/$DIX_PKG"
-            dix.log "Linked: $DIX_PKG${path/$DIX_PKG_PATH/}"
-        done
-        unset path dest
-
-        dix.log "Enabled: $DIX_PKG"
-    done
-}
-
-pkg.disable(){
-    local names pack DIX_PKG
-    names=$(pkg.__names $@) && eval $names || exit 1
-    for pack in "${names[@]}"; do
-        DIX_PKG="$(pkg.info "$pack")"
-        paths=$(find "$DIX_PATH" ! -type f ! -path "$DIX_PATH_OPT/*" -name "$DIX_PKG")
-        did=false
-        for path in $paths; do
-            rm -Rf "$path" && dix.log "Removed ${path/$DIX_PATH\//}" && did=true
-        done
-        $did && dix.log "Disabled: $DIX_PKG" || dix.log "Not found: $DIX_PKG"
-    done
-}
-
-pkg.boot(){
-    pkg.__boothalt boot $@
-}
-
-pkg.halt(){
-    pkg.__boothalt halt $@
-}
-
-pkg.install(){
-    local names pack name
-    names=$(pkg.__names $@) && eval $names || exit 1
-    for pack in "${names[@]}"; do
-        name="$(pkg.info "$pack")"
-        pkg.fetch "$name" && pkg.enable "$name" && pkg.boot "$name"
-    done
-}
-
-pkg.uninstall(){
-    local names pack name
-    names=$(pkg.__names $@) && eval $names || exit 1
-    for pack in "${names[@]}"; do
-        name="$(pkg.info "$pack")"
-        pkg.halt "$name" && pkg.disable "$name" && pkg.lose "$name"
-    done
+        # Install subdependencies:
+        # TODO: Handle uninstallation of dependencies
+        [[ "$DIX_PKG_TYPE"  == 'boot' && ${#DIX_REQUIRE[@]} -gt 0 ]] &&\
+            pkg.install ${DIX_REQUIRE[@]}
+        # Everything ready, run the script
+        source "$DIX_PKG_PATH/$DIX_PKG_TYPE"
+        type.is_func DIX_ON_AFTER && DIX_ON_AFTER
+    ); done
 }
