a9043439df35b4cd1c8fc1345012eae94ded0f2e
Héctor Menéndez <etor@gMac.local>
2016-04-19 02:49:32 -0500

    Implemented a string splitter

--- opt/dix/lib/string.sh
+++ opt/dix/lib/string.sh
@@ -17,3 +17,11 @@ string.length(){
 string.repeat(){
 	printf "$1%.0s" $(seq 1 $2)
 }
+
+# Splits a string ($1) using separator ($2) to a variable name ($3)
+string.split(){
+	local __array name
+	[[ -z "$3" ]] && name='__array' || name="$3"
+	IFS="$2" read -r -a __array <<< "$1"
+	declare -p __array | perl -pe "s/__array/$3/g"
+}
